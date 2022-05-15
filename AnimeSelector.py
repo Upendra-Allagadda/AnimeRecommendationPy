c09e7461c98bf25ecfec5739b5674282e8860cd4
Allagadda Sai Upendranath <sainath940@gmail.com>
2022-05-15 22:56:45 +0530

    test

--- AnimeSelector.py
+++ AnimeSelector.py
@@ -1,12 +1,15 @@
 import random
 AnimeList = []
-AnimeList = ["Naruto", "Boruto", "One Piece", "The Seven Deadly Sins", "Avatar-The last air bender", "Attack on Titans", "One Punch man", "Rick & Morth", "The Family Man"]
+AnimeList = ["Naruto", "Boruto", "One Piece", "The Seven Deadly Sins", 
+            "Avatar-The last air bender", "Attack on Titans", "One Punch man", "Rick & Morth", "The Family Man"]
 
 UserChoice = input("Do you need help selecting anime? [Y/N]:")
+yesList = ["Y","y", "Yes", "YES","yes"]
+noList = ["N","n", "NO", "No", "no"]
 
-if(UserChoice in ["Y","y", "Yes", "YES","yes"]):
+if UserChoice in yesList:
     print("Here is your anime to watch: " + random.choice(AnimeList))
-elif UserChoice in ["N","n", "NO", "No", "no"]:
+elif UserChoice in noList:
     print("Ok Bye!! Have a nice day!!")
 else:
-    print("Please enter a yes or no.")
+    print("Please enter yes or no.")
